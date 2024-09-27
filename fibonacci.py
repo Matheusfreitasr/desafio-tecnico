9334621cb5bc6cbc21f420746139e3779bdc0d64
Matheusfreitasr <133393868+Matheusfreitasr@users.noreply.github.com>
2024-09-27 17:05:45 -0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci_check(n):
+    a, b = 0, 1
+    fib_sequence = [a, b]
+    
+    while b < n:
+        a, b = b, a + b
+        fib_sequence.append(b)
+    
+    if n in fib_sequence:
+        return f"O número {n} pertence à sequência de Fibonacci."
+    else:
+        return f"O número {n} não pertence à sequência de Fibonacci."
+
+# Exemplo de uso:
+numero = int(input("Digite um número: "))
+print(fibonacci_check(numero))
